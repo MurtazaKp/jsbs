fbf33733daafcd23e88e08e4308a3c896ac41b64
MurtazaKp <Khopoliwala52@gmail.com>
2025-07-16 13:58:01 +0530

    feat(link)  : remove unsued link

--- components/Footer/CopyRight.js
+++ components/Footer/CopyRight.js
@@ -17,22 +17,7 @@ const CopyRight = () => {
                 All Rights Reserved
               </p>
             </div>
-            <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
-              <ul className="copyright-link rbt-link-hover justify-content-center justify-content-lg-end mt_sm--10 mt_md--10">
-                <li>
-                  <Link href="#">Terms of service</Link>
-                </li>
-                <li>
-                  <Link href="/privacy-policy">Privacy policy</Link>
-                </li>
-                <li>
-                  <Link href="/subscription">Subscription</Link>
-                </li>
-                <li>
-                  <Link href="/login">Login & Register</Link>
-                </li>
-              </ul>
-            </div>
+            <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12"></div>
           </div>
         </div>
       </div>
